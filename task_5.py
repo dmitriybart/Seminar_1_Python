f81e96637b5a21a92a1e6a7a0a26b7efcd3c930c
Dmitriy B <kupperfeet@gmail.com>
2022-09-30 19:18:08 +0300

    task 5 done

--- task_5.py
+++ task_5.py
@@ -4,7 +4,11 @@ import math
 
 number = float (input('Введите дробное число: '))
 num_result = number*10
-if num_result%10 == 0:
+if number - int(number) == 0:
     print('нет')
 else:
     print (math.floor(num_result)%10)
+
+# f = float(input())
+# d = int( (f*10) % 10 )
+# print(f, d, sep=" -> ")
