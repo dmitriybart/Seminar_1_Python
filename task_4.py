ef72c317a02c1c00da889a12d9e954d97af3cefc
Dmitriy B <kupperfeet@gmail.com>
2022-09-30 18:32:39 +0300

    task 4 done

---
+++ task_4.py
@@ -0,0 +1,8 @@
+# Напишите программу, которая будет на вход принимать число N и выводить числа от -N до N
+n = int(input('Введите число: '))
+# startNumber = -n
+# while (startNumber <= n):
+#     print(startNumber, end=" ")
+#     startNumber+=1
+for i in range(-n,n+1):
+    print(i, end=" ")
